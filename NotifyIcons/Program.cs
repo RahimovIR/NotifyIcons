d1e82f55604c386038617e14e56654696ad737e8
Ilyas Rahimov <rahimovir@gmail.com>
2015-10-16 17:22:52 +0500

    Init

---
+++ NotifyIcons/Program.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using NDesk.Options;
+
+namespace NotifyIcons
+{
+
+    class Program
+    {
+
+        static int Main(string[] args)
+        {
+            string exeName = null;
+            int preference = 2;
+            bool list = false;
+            bool show_help = false;
+
+            var options = new OptionSet()
+            {
+                {"n|name=", "Exec name",  
+                    v => exeName = v},
+                {"p=", "Change preference to. Default 2. \n0: Only show notifications. \n1: Hide icon and notifications. \n2: Show icon and notifications.", 
+                    (int v) => {if(v>=0 && v <=2) preference = v;}},
+                {"l", "List all notify items with preference.", 
+                    v => list = v != null},
+                { "h|help",  "show this message and exit", 
+                    v => show_help = v != null },
+            };
+
+            List<string> extra;
+            try
+            {
+                extra = options.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.Write("NotifyIcons: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `NotifyIcons --help' for more information.");
+                return 1;
+            }
+
+            if (show_help)
+            {
+                ShowHelp(options);
+                return 0;
+            }
+
+            if (list)
+            {
+                bool grep = !string.IsNullOrWhiteSpace(exeName);
+                RegistryKey myKey = Registry.CurrentUser.OpenSubKey(TrayHelper.trayNotifyPath, false);
+                byte[] all = (byte[])myKey.GetValue("IconStreams");
+                NotifyItem[] items = TrayHelper.getNotifyItems(all);
+                int count = 0;
+
+                if (grep)
+                {
+                    foreach (NotifyItem item in items)
+                    {
+                        if (item.ExePath.IndexOf(exeName, StringComparison.OrdinalIgnoreCase) > 0)
+                        {
+                            Console.WriteLine(string.Format("{0} preferenc: {1}", item.ExePath, item.preference));
+                            ++count;
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (NotifyItem item in items)
+                    {
+                        Console.WriteLine(string.Format("{0} preferenc: {1}", item.ExePath, item.preference));
+                        ++count;
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine("Count: " + count.ToString());
+
+                return 0;
+            }
+
+            if (!string.IsNullOrWhiteSpace(exeName))
+            {
+                RegistryKey myKey = Registry.CurrentUser.OpenSubKey(TrayHelper.trayNotifyPath, true);
+                byte[] all = (byte[])myKey.GetValue("IconStreams");
+                NotifyItem[] items = TrayHelper.getNotifyItems(all);
+                bool rewrite = false;
+                int count = 0;
+
+                List<int> changes = new List<int>();
+                for (int i = 0; i < items.Length; i++)
+                {
+                    if (items[i].ExePath.IndexOf(exeName, StringComparison.OrdinalIgnoreCase) > 0)
+                    {
+                        changes.Add(i);
+                    }
+                }
+                foreach (int change in changes)
+                {
+
+                    int offset = TrayHelper.getOffSet(change);
+                    if (items[change].preference != preference)
+                    {
+                        byte[] bb = BitConverter.GetBytes((int)(preference));
+                        Buffer.BlockCopy(bb, 0, all, offset, 4);
+                        rewrite = true;
+                        ++count;
+                    }
+                }
+                if (rewrite)
+                {
+                    myKey.SetValue("IconStreams", all);
+                }
+
+                Console.WriteLine("Change items: " + count.ToString());
+                return 0;
+            }
+
+            return 0;
+        }
+
+        static void ShowHelp(OptionSet p)
+        {
+            Console.WriteLine("Change notify icons visible status.");
+            Console.WriteLine("Usage: NotifyIcons [OPTIONS]");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            p.WriteOptionDescriptions(Console.Out);
+        }
+    }
+}
---
+++ NotifyIcons/TrayHelper.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotifyIcons
+{
+    //public enum NOTIFYITEM_PREFERENCE
+    //{
+    //    // In Windows UI: "Only show notifications."
+    //    PREFERENCE_SHOW_WHEN_ACTIVE = 0,
+    //    // In Windows UI: "Hide icon and notifications."
+    //    PREFERENCE_SHOW_NEVER = 1,
+    //    // In Windows UI: "Show icon and notifications."
+    //    PREFERENCE_SHOW_ALWAYS = 2
+    //};
+
+    public struct Header
+    {
+        public uint cbSize;
+        public uint unknown1;
+        public uint unknown2;
+        public uint count;
+        public uint unknown3;
+    }
+    // The known values for NOTIFYITEM's dwPreference member.
+    [StructLayout(LayoutKind.Sequential, Pack = 1, CharSet = CharSet.Unicode)]
+    public struct NotifyItem
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 528)]
+        public byte[] exe_path;
+
+        public int preference;
+
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 1108)]
+        public byte[] dontcare;
+
+        public unsafe string ExePath
+        {
+            get
+            {
+                byte[] exeCopy = new byte[exe_path.Length];
+
+                for (int i = 0; i < exe_path.Length; i++)
+                {
+                    byte byteToRot = exe_path[i];
+                    byte rot = new byte();
+                    if (byteToRot > 64 && byteToRot < 91)
+                    {
+                        rot = (byte)((byteToRot - 64 + 13) % 26 + 64);
+                    }
+                    else if (byteToRot > 96 && byteToRot < 123)
+                    {
+                        rot = (byte)((byteToRot - 96 + 13) % 26 + 96);
+                    }
+                    else
+                    {
+                        rot = byteToRot;
+                    }
+                    exeCopy[i] = rot;
+                }
+
+                fixed (byte* b = exeCopy)
+                {
+                    return Marshal.PtrToStringUni((IntPtr)b);
+                }
+            }
+        }
+
+    };
+
+
+    class TrayHelper
+    {
+        static int headerSize = 20;
+        static int blockSize = 1640;
+        static public string trayNotifyPath = @"Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify";
+
+
+        static NotifyItem Byte2NotifyItem(byte[] bytes)
+        {
+            int size = Marshal.SizeOf(typeof(NotifyItem));
+            var ptr = Marshal.AllocHGlobal(size);
+            Marshal.Copy(bytes, 0, ptr, size);
+            NotifyItem result = (NotifyItem)Marshal.PtrToStructure(ptr, typeof(NotifyItem));
+
+            return result;
+        }
+
+        static Header Byte2Header(byte[] bytes)
+        {
+            int size = Marshal.SizeOf(typeof(Header));
+            var ptr = Marshal.AllocHGlobal(size);
+            Marshal.Copy(bytes, 0, ptr, size);
+            Header result = (Header)Marshal.PtrToStructure(ptr, typeof(Header));
+
+            return result;
+        }
+
+        public static int getOffSet(int id)
+        {
+            return headerSize + id * blockSize + 528;
+        }
+
+        public static NotifyItem[] getNotifyItems(byte[] bytes)
+        {
+            Header header = Byte2Header(bytes.Take(headerSize).ToArray());
+
+            NotifyItem[] items = new NotifyItem[header.count];
+            for (int i = 0; i < header.count; i++)
+            {
+                byte[] blockByte = bytes.Skip(headerSize + i * blockSize).Take(blockSize).ToArray();
+                items[i] = Byte2NotifyItem(blockByte);
+            }
+
+            return items;
+        }
+    }
+}
